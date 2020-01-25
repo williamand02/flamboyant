b96af0897f689f522895e6fd10b8a11332e7d0e3
william andrade <williamstrayder@gmail.com>
2020-01-25 20:19:10 -0300

    test 2
    testando o commit novamente

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Aprendendo com CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>aqui começa a magica</h2>
     </div>
   );
 }
