93fa681ceb5c3e738d6025fb5357229ee09f3a30
william andrade <williamstrayder@gmail.com>
2020-01-25 20:17:04 -0300

    test
    testando o commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Aprendendo com CodeSandbox</h1>
+      <h1>Aprendendo com CodeSandbox2</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
