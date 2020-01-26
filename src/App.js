88b329ed0ac22e5df6258824dd92e3b7d90110d8
william andrade <williamstrayder@gmail.com>
2020-01-25 21:28:22 -0300

    comimiiitttt
    comimiiitttt

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Aprendendo com CodeSandbox2</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>inicio</h2>
     </div>
   );
 }
